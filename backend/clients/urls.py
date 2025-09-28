a7ad33b1324016b17e5d5e55fe2610b59b855ba5
Henok <49408901+henokcreative@users.noreply.github.com>
2025-09-28 17:43:00 +0300

    Add clients API urls

--- backend/clients/urls.py
+++ backend/clients/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ClientListCreateView  # adjust this to your DRF view
 
 urlpatterns = [
-    path('', ClientListCreateView.as_view(), name='client-list'),
+    path('clients/', ClientListCreateView.as_view(), name='client-list-create'),
 ]
