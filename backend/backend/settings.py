8804a67da8dc44cd2369cd3b6295b146cc197b4c
Henok <49408901+henokcreative@users.noreply.github.com>
2025-09-28 23:20:16 +0300

    updated databases

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,9 +75,10 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
-        conn_max_age=600
+    "default": dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",  # fallback for local dev
+        conn_max_age=600,
+        ssl_require=True  # ensures SSL connection to Postgres
     )
 }
 
