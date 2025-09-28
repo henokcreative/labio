81effb03b1ac4b9a17e1f9db0770dc398bc606a8
Henok <49408901+henokcreative@users.noreply.github.com>
2025-09-28 17:33:43 +0300

    Add clients API urls

--- backend/clients/views.py
+++ backend/clients/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-
+from rest_framework import generics, viewsets
 from rest_framework import viewsets
 from .models import Message, Contract
 from .serializers import MessageSerializer, ContractSerializer
