bed65faea77a5e13fa50f4a3701f1d274bb50980
Henok <49408901+henokcreative@users.noreply.github.com>
2025-12-08 10:21:36 +0200

    dashboard-test

--- backend/dashboard/models.py
+++ backend/dashboard/models.py
@@ -1,11 +1,9 @@
 from django.db import models
-
 class Message(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField()
     message = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=20, default="New")
-
     def __str__(self):
         return f"{self.name} - {self.email}"
